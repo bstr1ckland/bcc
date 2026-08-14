aff9743a2998b4874fb37b8476cc1b298a5bf73f
bstr1ckland <ben.strickland@dal.ca>
2026-08-13 21:04:43 -0300

    committing lexical anaylizer changes

--- src/lexical/tokens.rs
+++ src/lexical/tokens.rs
@@ -1,7 +1,19 @@
+
+
 pub enum TokenKind {
     // Single char tokens
+    LeftBrace,        // {
+    RightBrace,       // }
+    LeftParenthesis,  // (
+    RightParenthesis, // )
+    LeftBracket,      // [
+    RightBracket,     // ]
+    SemiColon,        // ;
+    Colon,            // :
 
     // Literals
 
     // Keywords
+    Void,
+    Include,
 }
